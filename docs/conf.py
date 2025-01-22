2daa6e73131addfc39d6855ae828db423a20902d
Lazaro Osee <odiraoseelazaro@students.tukenya.ac.ke>
2025-01-22 23:10:59 +0300

    Added sphinx with python

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+# filepath: /d:/Sites/libraries/react-hooks/package/docs/conf.py
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+
+project = 'react-hooks'
+copyright = '2025, Lazaro Osee'
+author = 'Lazaro Osee'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_js',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+js_source_path = '../src'
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. react-hooks documentation master file, created by
+   sphinx-quickstart on Wed Jan 22 23:03:19 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+react-hooks documentation
+=========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
